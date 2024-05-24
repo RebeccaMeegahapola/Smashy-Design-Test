18e9a979dbd6579b2befb8f69e40fb38e4e2d9b4
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2024-05-24 16:26:37 +0530

    up

--- Home-Page.js
+++ Home-Page.js
@@ -1,4 +1,39 @@
-const swiper = new Swiper('.swiper', {
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: true,
+//     slidesPerView: 2,
+//     autoplay: {
+//         delay: 1000,
+//         disableOnInteraction: false,
+//       },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.button-next',
+//     },
+
+//     breakpoints: {
+//         // when window width is <= 320px
+//         320: {
+//           slidesPerView: 1,
+//           spaceBetween: 5
+//         },
+//         // when window width is <= 480px
+//         425: {
+//           slidesPerView: 2,
+//           spaceBetween: 10
+//         },
+//         // when window width is <= 640px
+//         // 768: {
+//         //   slidesPerView: 2,
+//         //   spaceBetween: 20
+//         // }
+//       }
+  
+//   });
+
+  const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -6,29 +41,45 @@ const swiper = new Swiper('.swiper', {
     autoplay: {
         delay: 1000,
         disableOnInteraction: false,
-      },
-  
+    },
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
+        nextEl: '.button-next',
+    },
+
+    // Pagination (enabled by default)
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        // Disable pagination by default
+        enabled: false,
     },
 
     breakpoints: {
         // when window width is <= 320px
         320: {
-          slidesPerView: 1,
-          spaceBetween: 5
+            slidesPerView: 1,
+            spaceBetween: 5,
+            pagination: {
+                enabled: true,
+            },
         },
         // when window width is <= 480px
         425: {
-          slidesPerView: 2,
-          spaceBetween: 10
+            slidesPerView: 2,
+            spaceBetween: 10,
+            pagination: {
+                enabled: true,
+            },
         },
         // when window width is <= 640px
-        // 768: {
-        //   slidesPerView: 2,
-        //   spaceBetween: 20
-        // }
-      }
-  
-  });
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+          pagination: {
+            enabled: false,
+        },
+        }
+    },
+});
