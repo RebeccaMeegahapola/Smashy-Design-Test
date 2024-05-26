a9324ce5871d8fe8d58c2d69c3a9831d9b474e1f
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2024-05-26 14:53:53 +0530

    up

--- Home-Page.js
+++ Home-Page.js
@@ -1,39 +1,5 @@
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'horizontal',
-//     loop: true,
-//     slidesPerView: 2,
-//     autoplay: {
-//         delay: 1000,
-//         disableOnInteraction: false,
-//       },
-  
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.button-next',
-//     },
-
-//     breakpoints: {
-//         // when window width is <= 320px
-//         320: {
-//           slidesPerView: 1,
-//           spaceBetween: 5
-//         },
-//         // when window width is <= 480px
-//         425: {
-//           slidesPerView: 2,
-//           spaceBetween: 10
-//         },
-//         // when window width is <= 640px
-//         // 768: {
-//         //   slidesPerView: 2,
-//         //   spaceBetween: 20
-//         // }
-//       }
-  
-//   });
-
-  const swiper = new Swiper('.swiper', {
+//slider
+const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -83,3 +49,9 @@
         }
     },
 });
+
+$(document).ready(function() {
+    $("#dropdownNav").click(function() {
+        $(".dropdownNavItems").toggleClass("active");
+     });
+    });
